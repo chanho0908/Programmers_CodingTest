cea5f87424fd9c00250fdf41afac2db114e41d0b
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-01-21 13:57:49 +0900

    [level 0] Title: A 강조하기, Time: 9.36 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(char x:myString.toCharArray()){
+            if(x=='a') answer += "A";
+            else if(x != 'A' && Character.isUpperCase(x)) 
+                answer += Character.toLowerCase(x);
+            else answer += x;
+        }
+            
+            
+        return answer;
+    }
+}

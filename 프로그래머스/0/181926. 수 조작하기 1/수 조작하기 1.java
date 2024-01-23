7e969f27cb474441b246742ee40e7dc7513290c2
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-01-23 15:03:47 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.64 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(char x : control.toCharArray()){
+            switch(x){
+                case 'w' : n+=1; break;
+                case 's' : n-=1; break;
+                case 'd' : n+=10; break;
+                case 'a' : n-=10; break;    
+            }
+                
+        }
+            
+        return n;
+    }
+}

2bd8ce6958577dad22e00e084c5d665e7915d9c2
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-02-07 14:04:36 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = new StringBuilder(my_string).reverse().toString();
+        
+        return answer;
+    }
+}

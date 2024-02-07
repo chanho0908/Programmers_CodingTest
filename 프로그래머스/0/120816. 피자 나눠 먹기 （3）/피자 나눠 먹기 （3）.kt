e528cd9daf2532d4a00c78a4341011b3c237d264
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-02-08 00:52:04 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
@@ -1,12 +1,5 @@
 class Solution {
-    fun solution(slice: Int, n: Int): Int {
-        var answer: Int = 
-        if(n % slice == 0){
-            n / slice
-        }else{
-            n / slice + 1
-        }
-        
-        return answer
-    }
+   fun solution(slice: Int, n: Int): Int = 
+    if(n % slice == 0){ n / slice } else{ n / slice + 1 }
+    
 }

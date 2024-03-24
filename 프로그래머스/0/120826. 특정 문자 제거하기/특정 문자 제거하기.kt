88807b54cfe9545ea4ad37ba7ed5267c8f3566ec
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-03-25 01:13:48 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 11.27 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, letter: String): String {
+     return my_string.toCharArray().filter { 
+         it.toString() != letter 
+     }.joinToString("")
+    }
+}

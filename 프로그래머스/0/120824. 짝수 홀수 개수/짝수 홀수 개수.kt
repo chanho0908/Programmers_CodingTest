c7f6e87f30a91320a59eaef58b70ce65aa4865c7
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-05-13 10:03:22 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,11 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = intArrayOf(0, 0)
-        num_list.map{
-            if (it % 2 == 0) answer[0]++
-            else answer[1]++
-        }
-
-        return answer
+        val x = num_list.count { it % 2 == 0 }
+        val y = num_list.count { it % 2 != 0 }
+        return intArrayOf(x,y)
     }
 }

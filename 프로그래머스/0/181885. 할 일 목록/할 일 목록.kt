4c72d1f652c544cdac8c8d9ea8e5d85064fb5dcd
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-05-13 12:08:22 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
+    return todo_list.filterIndexed{idx, _ ->
+        !finished[idx]
+    }.toTypedArray()
+}
+
+}

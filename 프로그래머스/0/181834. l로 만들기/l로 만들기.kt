5dcdd775bf46f3d3e4226ca41b2a8631b921f869
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-05-22 02:09:53 +0900

    [level 0] Title: l로 만들기, Time: 27.13 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(myString: String): String {
+        return myString.map {
+            if(it.code < 'l'.code) "l"
+            else it
+        }.joinToString("")   
+    }
+}

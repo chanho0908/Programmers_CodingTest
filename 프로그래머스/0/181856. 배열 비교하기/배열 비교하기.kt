e1b78d00580e336bc251366c8f0a08058115c921
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-05-22 22:40:36 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(arr1: IntArray, arr2: IntArray): Int = if(arr1.size == arr2.size){
+        if(arr1.sum() == arr2.sum()){
+            0
+        }else if(arr1.sum() > arr2.sum()){
+            1
+        }else{
+            -1
+        }
+    }else{
+        if(arr1.size > arr2.size) 1
+        else -1
+    }
+}

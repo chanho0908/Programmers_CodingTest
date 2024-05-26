eae22d9230975fd32f50a753cd55ff59ec6120cf
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-05-26 19:08:24 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, k: Int): String {
+        val sb = StringBuilder()
+        repeat(k){ sb.append(my_string) }
+        return sb.toString()
+    }
+}

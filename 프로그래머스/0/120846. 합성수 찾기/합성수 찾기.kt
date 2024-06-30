1dac72649319fb9050d9a37223f1e81111f12097
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-07-01 04:52:37 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+    
+        for(i in 1..n){
+            var cnt = 0
+            for(j in 1..i){
+                if(i % j == 0) cnt++
+            }
+            if(cnt >= 3) answer++
+        }
+        return answer
+    }
+}

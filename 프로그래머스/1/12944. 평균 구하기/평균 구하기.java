b900e3f9468b15baa07375a22634ce7f90b25edc
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-07-23 13:57:21 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i : arr){
+            answer += (double) i;
+        }
+        return answer / arr.length;
+    }
+}

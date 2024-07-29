382af4416a79e7fd46bad2f16744abbd30e400b0
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-07-29 16:09:57 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 61.9 MB, 시간: 8.45 ms
+메모리: 76.9 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 15:30:55
+2024년 07월 29일 16:09:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> workDays = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            int cnt = 0;
+            while (progresses[i] < 100) {
+                progresses[i] += speeds[i];
+                cnt++;
+            }
+            workDays.add(cnt);
+        }
+
+        int lt = 0;
+        int rt = 0;
+        int works = 1;
+        while (lt != workDays.size() - 1) {
+            if (workDays.get(rt) >= workDays.get(lt + 1)) {
+                works++;
+            } else {
+                answer.add(works);
+                rt = lt + 1;
+                works = 1;
+            }
+            lt++;
+        }
+        answer.add(works);
         return answer;
     }
 }

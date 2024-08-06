bb10f6daeeb672856f41aa695b9c3a91e821fa19
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-08-06 14:35:16 +0900

    [level 1] Title: 최소직사각형, Time: 33.00 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -3,21 +3,14 @@ import kotlin.math.min
 
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-    for(arr in sizes){
-        if (arr[0] < arr[1]){
-            val swap = arr[0]
-            arr[0] = arr[1]
-            arr[1] = swap
+        var maxN = 0
+        var minN = 0
+        for (arr in sizes){
+            val s = arr.sortedDescending()
+            maxN = max(maxN, s[0])
+            minN = max(minN, s[1])
         }
-    }
 
-    var maxN = 0
-    var minN = 0
-    for (arr in sizes){
-        maxN = max(maxN, arr[0])
-        minN = max(minN, arr[1])
+        return maxN * minN
     }
-
-    return maxN * minN
-}
 }

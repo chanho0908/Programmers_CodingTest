b1b65cada8e95d8ced86fcafb345f79cd3019c45
정찬호 <84930748+chanho0908@users.noreply.github.com>
2024-10-08 21:58:08 +0900

    [Silver III] Title: N과 M (6), Time: 152 ms, Memory: 24172 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.kt
@@ -0,0 +1,37 @@
+private lateinit var arr: IntArray
+private lateinit var numbersArray: IntArray
+private lateinit var visited: BooleanArray
+private var N = 0
+private var M = 0
+
+fun main(){
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    N = n
+    M = m
+    arr = IntArray(N)
+    visited = BooleanArray(N)
+    numbersArray = IntArray(M)
+
+    val numbers = readln().split(" ").map { it.toInt() }
+    numbers.sorted().forEachIndexed{ i, v->
+        arr[i] = v
+    }
+
+    BackTracking(0, 0)
+}
+
+private fun BackTracking(start: Int, depth: Int){
+    if (depth == M){
+        println(numbersArray.joinToString(" "))
+        return
+    }
+
+    for (i in start until  N){
+        if (visited[i].not()) {
+            visited[i] = true
+            numbersArray[depth] = arr[i]
+            BackTracking(i, depth + 1)
+            visited[i] = false
+        }
+    }
+}
